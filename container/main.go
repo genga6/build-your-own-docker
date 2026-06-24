6a1b2bb704895d7f6a92309782dd6d89c2254e7a
Takumi Matsuzawa <152503584+genga6@users.noreply.github.com>
2026-06-24 12:59:29 +0000

    feat: add syscall configurations for child process and hostname setting

--- container/main.go
+++ container/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 // 使い方: go run . run <コマンド> [引数...]
@@ -26,7 +27,7 @@ func main() {
 	case "child":
 		child()
 	default:
-		panic("unknown command: " + os.Args[1])	
+		panic("unknown command: " + os.Args[1])
 	}
 }
 
@@ -34,11 +35,31 @@ func main() {
 func run() {
 	fmt.Printf("[run]	pid=%d args=%v\n", os.Getpid(), os.Args[2:])
 
+	// ステップ0：自分自身を別プロセスとして起動しなおす。
 	// "/proc/self/exe" = 今まさに動いている自分自身の実行ファイルを指すカーネルのリンク。
 	// append([]string{"child"}, os.Args[2:]...) で
 	//   ["child", "echo", "hello"] のような引数列を組み立てる。
 	//   os.Args[2:] は "run" の後ろ全部（例: echo hello）。
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+
+	// ステップ1：新しい名前空間つきで子プロセスを起動する設定。
+	// ここでは「プロセスIDの名前空間」を作る。つまり、子プロセスは自分が PID=1 だと思う。
+	// これにより、子プロセスは自分のプロセスツリーの中で init プロセスのように振る舞える。
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUSER | // rootlessの起点
+			syscall.CLONE_NEWUTS | // ホスト名を隔離
+			syscall.CLONE_NEWPID | // PID を隔離
+			syscall.CLONE_NEWNS, // マウントを隔離
+
+		// rootless のための UID/GID マッピングを設定する。
+		// 「中のUIDを外のどのUIDに対応させるか」の翻訳表
+		UidMappings: []syscall.SysProcIDMap{
+			{ContainerID: 0, HostID: os.Getuid(), Size: 1}, // 子プロセスの root(0) を、親の自分の UID に対応させる
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{ContainerID: 0, HostID: os.Getgid(), Size: 1}, // 子プロセスの root(0) を、親の自分の GID に対応させる
+		},
+	}
 
 	// 標準入出力を親（このプロセス）とつなぎ、
 	// child の出力が見る／入力を渡す。
@@ -53,6 +74,9 @@ func run() {
 // child: 起動し直された側。ここで目的のコマンドを実行する。
 func child() {
 	fmt.Printf("[child] pid=%d args=%v\n", os.Getpid(), os.Args[2:])
+
+	// UTS namespace 仲なので、ここでホスト名を変えてもホスト側には影響しない。
+	must(syscall.Sethostname([]byte("container")))
 
 	// os.Args[2:] = 目的のコマンドとその引数（例: echo hello）。
 	// os.Args[2]  = "echo"（実行するコマンド）
